49e9b5494a594e55261dc1a8036d4e77f4a59fea
geovanex24 <geovanex24@hotmail.com>
2024-09-02 12:36:49 -0600

    bonus: RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { PokemonApp } from "./PokemonApp.jsx";
+import { TodoApp } from "./TodoApp.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 // import App from "./App.jsx";
+// import { PokemonApp } from "./PokemonApp.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </StrictMode>
 );
